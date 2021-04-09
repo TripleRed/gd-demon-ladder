9be5a3b8178ec0748616127a2055f2e7327f4cc4
TripleRed <mickeychan722@hotmail.com>
2021-04-09 19:52:35 +0800

    Newly rated levels data can be generated but it requires ID input

--- newrates.py
+++ newrates.py
@@ -6,3 +6,60 @@
 # because I just have to lazily copy and paste.
 
 # Comments refer to the previous line except for headers, marked with *
+
+# ** Setup **
+import requests
+
+# ** Prompt to delete existing levels.json **
+print('gdl> Would you wish to delete the current JSON file?')
+print('(Y/N, Y if any other response is given)', end=' ')
+boolstr = input()
+if boolstr != 'N': # initiates deletion unless N is given
+    fileedit = open("levels.json","w",encoding="utf-8")
+    fileedit.write('')
+    fileedit.close()
+
+# ** Obtain data from ID **
+
+firstitem = True
+
+# * open file for editing *
+fileedit = open("levels.json","a",encoding="utf-8")
+fileedit.write('[')
+    # I'll be removing the first [ in the loop so I'll need to add that
+
+# * The forever loop *
+while True:
+    # * url setup *
+    id_in_question = str(input('gdl> Input ID number (enter nothing to end): '))
+    if id_in_question == '':
+        print('gdl> End input detected. Exiting.')
+        break
+    else: print('gdl> Attempting to obtain level data with ID ' + id_in_question)
+    url = "https://gdbrowser.com/api/level/" + id_in_question
+    leveldata = requests.get(url) # request from website
+    leveltext = leveldata.text
+        # obtained data is not human-readable so need to convert
+        # I chose to use a new var to store the original data
+
+    # * detect gdbrowser error *
+    if leveltext == '-1':
+        print('\ngdl> Bad response from gdbrowser detected.')
+        print('gdl> Requests will now stop.')
+        break # stop requesting new data
+
+    # * writes the comma for second item onwards *
+    if firstitem == False: fileedit.write(',')
+    firstitem = False
+
+    # * write into file *
+    fileedit.write(leveltext)
+
+fileedit.write('\n]') # same thing as the [ but at the end
+fileedit.close()
+
+############# end of get serious backdoor moment #############
+
+# ** End prompt **
+print('gdl> Check levels.json for output.')
+print('gdl> Ending program.')
